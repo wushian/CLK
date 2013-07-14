e45764d7e7329f33ac4592acc214276f3a8405f5
Clark159 <clark159@hotmail.com>
2013-07-14 14:10:13 +0800

    重構CLK.Settings.Samples讓它更聚焦於切換SettingComtext

--- Sources/CLK.Settings/CLK.Settings.Samples.WinConsole/Program.cs
+++ Sources/CLK.Settings/CLK.Settings.Samples.WinConsole/Program.cs
@@ -12,46 +12,50 @@ namespace CLK.Settings.Samples.WinConsole
         public static void Main(string[] args)
         {
             // Sample
-            ConfigSample();
-            MemorySample();
+            ConfigSettingsSample();
+            MemorySettingsSample();
 
             // End
             Console.ReadLine();
         }
+                
 
-        private static void ConfigSample()
+        private static void ConfigSettingsSample()
         {
-            // Title
-            Console.WriteLine("Config Sample");
-
             // Initialize
             SettingContext.Current = new CLK.Settings.Configuration.ConfigSettingContext();
 
-            // Operate
-            Entity entity = EntityFactory.Create();
-            Console.WriteLine("Property001 : " + entity.Property001);
-            Console.WriteLine("Property002 : " + entity.Property002);
-
-            // End
-            Console.WriteLine();
+            // Run
+            Run("Config Settings Sample");
         }
 
-        private static void MemorySample()
+        private static void MemorySettingsSample()
         {
-            // Title
-            Console.WriteLine("Memory Sample");
-
             // Initialize
             SettingContext.Current = new CLK.Settings.MemorySettingContext();
             CLK.Settings.SettingContext.Current.AppSettings["AppSetting001"] = "AppSetting from memory";
             CLK.Settings.SettingContext.Current.ConnectionStrings["ConnectionString001"] = "ConnectionStrings from memory";
 
+            // Run
+            Run("Memory Settings Sample");
+        }
+
+
+        private static void Run(string title)
+        {
+            #region Contracts
+
+            if (string.IsNullOrEmpty(title) == true) throw new ArgumentException();
+
+            #endregion
+
+            // Title
+            Console.WriteLine(title);
+
             // Operate
             Entity entity = EntityFactory.Create();
             Console.WriteLine("Property001 : " + entity.Property001);
             Console.WriteLine("Property002 : " + entity.Property002);
-
-            // End
             Console.WriteLine();
         }
     }
