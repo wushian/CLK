94cf4a2202dc04c717de1d6da5a707eb1d899f08
Clark159 <clark159@hotmail.com>
2013-07-28 14:08:57 +0800

    加入FreeAttributeConfigurationElementCollection類別

--- Sources/CLK.Reflection/CLK.Reflection.Configuration/ReflectConfigurationElementCollection.cs
+++ Sources/CLK.Reflection/CLK.Reflection.Configuration/ReflectConfigurationElementCollection.cs
@@ -4,32 +4,13 @@ using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CLK.Configuration;
 
 namespace CLK.Reflection.Configuration
 {
-    public sealed class ReflectConfigurationElementCollection : ConfigurationElementCollection
+    public sealed class ReflectConfigurationElementCollection : FreeAttributeConfigurationElementCollection<ReflectConfigurationElement>
     {
-        // Constructors
-        public ReflectConfigurationElementCollection() { }
-
-
-        // Properties
-        public override ConfigurationElementCollectionType CollectionType
-        {
-            get
-            {
-                return ConfigurationElementCollectionType.AddRemoveClearMap;
-            }
-        }
-
-
         // Methods  
-        protected override ConfigurationElement CreateNewElement()
-        {
-            // Base
-            return new ReflectConfigurationElement();
-        }
-
         protected override object GetElementKey(ConfigurationElement element)
         {
             #region Contracts
@@ -40,62 +21,6 @@ namespace CLK.Reflection.Configuration
 
             // Base
             return ((ReflectConfigurationElement)element).EntityName;
-        }
-
-
-        public void Add(ReflectConfigurationElement element)
-        {
-            #region Contracts
-
-            if (element == null) throw new ArgumentNullException();
-
-            #endregion
-
-            // Base
-            this.BaseAdd(element);
-        }
-
-        public void Remove(string name)
-        {
-            #region Contracts
-
-            if (string.IsNullOrEmpty(name) == true) throw new ArgumentNullException();
-
-            #endregion
-
-            // Base
-            this.BaseRemove(name);
-        }
-
-        public bool Contains(string name)
-        {
-            #region Contracts
-
-            if (string.IsNullOrEmpty(name) == true) throw new ArgumentNullException();
-
-            #endregion
-
-            // Base
-            if (this.BaseGet(name) != null)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        public ReflectConfigurationElement GetByName(string name)
-        {
-            #region Contracts
-
-            if (string.IsNullOrEmpty(name) == true) throw new ArgumentNullException();
-
-            #endregion
-
-            // Base
-            return (ReflectConfigurationElement)this.BaseGet(name);
         }
     }
 }
