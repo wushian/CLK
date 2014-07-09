9de603a6359fe090f0377eac59c4d7616adc9e1c
Clark <clark159@hotmail.com>
2014-07-09 16:31:51 +0800

    加入EmptyAction

--- Sources/Domains/CLK/Scheduling/Implementation/Triggers/PromptlyTrigger.cs
+++ Sources/Domains/CLK/Scheduling/Implementation/Triggers/PromptlyTrigger.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace CLK.Scheduling.Implementation
+namespace CLK.Scheduling
 {
     public sealed class PromptlyTrigger: ITaskTrigger
     {
