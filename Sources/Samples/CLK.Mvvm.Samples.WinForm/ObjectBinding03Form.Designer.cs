c67b49c3d39e87ee16dfd53630a29b27d801d0b7
Clark <clark159@hotmail.com>
2016-01-21 11:05:39 +0800

    Merge CLK.Mvvm

---
+++ Sources/Samples/CLK.Mvvm.Samples.WinForm/ObjectBinding03Form.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace CLK.Mvvm.Samples.WinForm
+{
+    partial class ObjectBinding03Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Button1 = new System.Windows.Forms.Button();
+            this.Button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TextBox1
+            // 
+            this.TextBox1.Location = new System.Drawing.Point(13, 64);
+            this.TextBox1.Name = "TextBox1";
+            this.TextBox1.Size = new System.Drawing.Size(558, 26);
+            this.TextBox1.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Consolas", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(560, 52);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Object Binding 03 : ViewModel <-> View";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Button1
+            // 
+            this.Button1.Location = new System.Drawing.Point(13, 98);
+            this.Button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(558, 37);
+            this.Button1.TabIndex = 5;
+            this.Button1.Text = "Update ViewModel.Data";
+            this.Button1.UseVisualStyleBackColor = true;
+            this.Button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Button2
+            // 
+            this.Button2.Location = new System.Drawing.Point(13, 145);
+            this.Button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Button2.Name = "Button2";
+            this.Button2.Size = new System.Drawing.Size(558, 37);
+            this.Button2.TabIndex = 8;
+            this.Button2.Text = "Display ViewModel.Data";
+            this.Button2.UseVisualStyleBackColor = true;
+            this.Button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // ObjectBinding03Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 200);
+            this.Controls.Add(this.Button2);
+            this.Controls.Add(this.TextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Button1);
+            this.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "ObjectBinding03Form";
+            this.Text = "ObjectBinding03Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Button1;
+        private System.Windows.Forms.Button Button2;
+    }
+}
