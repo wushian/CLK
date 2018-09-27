fd3ba742b6864c017cbd09da5ab79dbf142ed06b
Clark <clark159@hotmail.com>
2018-09-27 11:48:35 +0800

    重構CLK.Logging

---
+++ src2/CLK.Logging.Log4net/Log4netLoggerContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLK.Logging.Log4net
+{
+    public class Log4netLoggerContext : LoggerContext
+    {
+        // Constructors
+        public Log4netLoggerContext(string configFilename = null) : base(new Log4netLoggerFactory(configFilename))
+        {
+
+        }
+    }
+}
---
+++ src2/CLK.Logging/LoggerFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLK.Logging
+{
+    public interface LoggerFactory : IDisposable
+    {
+        // Methods
+        Logger Create(Type target);
+    }
+}
